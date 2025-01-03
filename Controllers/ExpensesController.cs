6bcf3baecb769dbdf44459b91756f19bdb190e49
Dharti Thakkar <dharti.thakkar@marutitech.com>
2025-01-03 14:30:22 +0530

    Added new controller expense controller

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+namespace NetCoreApp.Controllers;
+
+[ApiController]
+[Route("expenses")]
+public class ExpensesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ExpensesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
