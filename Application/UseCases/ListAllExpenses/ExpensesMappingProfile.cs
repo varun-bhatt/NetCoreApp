c456d2b24889b991dcec0c096bc9c51f81722b1b
Dharti Thakkar <dharti.thakkar@marutitech.com>
2025-01-03 16:27:59 +0530

    updated list all expenses

--- Application/UseCases/ListAllExpenses/ExpensesMappingProfile.cs
+++ Application/UseCases/ListAllExpenses/ExpensesMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Peddle.Foundation.Common.Pagination;
 
 namespace NetCoreApp.Application.UseCases.ListAllExpenses;
 
@@ -6,23 +7,6 @@ public class ExpensesMappingProfile : Profile
 {
     public ExpensesMappingProfile()
     {
-        CreateMap<GetExpensesQuery, GetExpensesFilterModel>()
-            .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.GetExpensesRequest.StartDate))
-            .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => src.GetExpensesRequest.EndDate))
-            .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.GetExpensesRequest.Category))
-            .ForMember(dest => dest.MinAmount, opt => opt.MapFrom(src => src.GetExpensesRequest.MinAmount))
-            .ForMember(dest => dest.MaxAmount, opt => opt.MapFrom(src => src.GetExpensesRequest.MaxAmount))
-            .ForMember(dest => dest.SortBy, opt => opt.MapFrom(src => src.GetExpensesRequest.SortBy))
-            .ForMember(dest => dest.SortOrder, opt => opt.MapFrom(src => src.GetExpensesRequest.SortOrder))
-            .ForMember(dest => dest.PageNumber, opt => opt.MapFrom(src => src.GetExpensesRequest.PageNumber))
-            .ForMember(dest => dest.PageSize, opt => opt.MapFrom(src => src.GetExpensesRequest.PageSize))
-            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.GetExpensesRequest.UserId));
-
-        CreateMap<GetExpensesResponseModel, GetExpensesResponseDto>()
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
-            .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount))
-            .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
-            .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Date));
+        //CreateMap<PagedList<GetExpensesResponseDto>, PaginationMetadata>();
     }
 }
--- Application/UseCases/ListAllExpenses/GetExpensesQueryEventHandler.cs
+++ Application/UseCases/ListAllExpenses/GetExpensesQueryEventHandler.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using MediatR;
 using NetCoreApp.Application.Interfaces.Repositories;
 using Peddle.Foundation.Common.Dtos;
+using Peddle.Foundation.Common.Extensions;
 using Peddle.Foundation.Common.Pagination;
 using Peddle.Offer.Extensions;
 
@@ -23,19 +24,53 @@ namespace NetCoreApp.Application.UseCases.ListAllExpenses
 
         public async Task<Result<IEnumerable<ExpandoObject>, Exception>> Handle(GetExpensesQuery request, CancellationToken cancellationToken)
         {
-            var getExpensesFilterModel = _mapper.Map<GetExpensesFilterModel>(request.GetExpensesRequest);
+            var getExpensesFilterModel = new GetExpensesFilterModel
+            {
+                SortBy = request.GetExpensesRequest.SortBy,
+                StartDate = !request.GetExpensesRequest.StartDate.IsEmpty() ? Convert.ToDateTime(request.GetExpensesRequest.StartDate) : null,
+                SortOrder = request.GetExpensesRequest.SortOrder,
+                PageNumber = request.GetExpensesRequest.PageNumber,
+                EndDate = !request.GetExpensesRequest.EndDate.IsEmpty() ? Convert.ToDateTime(request.GetExpensesRequest.EndDate) : null,
+                Category = request.GetExpensesRequest.Category,
+                PageSize = request.GetExpensesRequest.PageSize,
+                UserId = request.GetExpensesRequest.UserId,
+                MinAmount = request.GetExpensesRequest.MinAmount,
+                MaxAmount = request.GetExpensesRequest.MaxAmount,
+            };
             var expenses = _expenseRepository.GetAll(getExpensesFilterModel);
             return await ShapeTheResponseData(expenses);
         }
 
         private async Task<Result<IEnumerable<ExpandoObject>, Exception>> ShapeTheResponseData(PagedList<GetExpensesResponseModel> getExpenses)
         {
-            var response = _mapper.Map<List<GetExpensesResponseDto>>(getExpenses);
+            var response = new List<GetExpensesResponseDto>();
+
+            foreach (var expense in getExpenses)
+            {
+                var expenseDto = new GetExpensesResponseDto
+                {
+                    Name = expense.Name,
+                    Description = expense.Description,
+                    Amount = expense.Amount,
+                    Category = expense.Category,
+                    Date = expense.Date
+                };
+                response.Add(expenseDto);
+            }
             var expensesShapedData = response.ShapeData(null);
-            var obj = new ExpandoObject();
-            ((IDictionary<string, object>)obj!).Add("Pagination", _mapper.Map<PaginationMetadata>(getExpenses));
+            // var obj = new ExpandoObject();
+            // ((IDictionary<string, object>)obj!).Add("Pagination", _mapper.Map<PaginationMetadata>(getExpenses));
+            // ((IDictionary<string, object>)obj!).Add("Pagination", new PaginationMetadata
+            // {
+            //     TotalCount = getExpenses.TotalCount,
+            //     PageSize = getExpenses.PageSize,
+            //     CurrentPage = getExpenses.CurrentPage,
+            //     TotalPages = getExpenses.TotalPages,
+            //     HasNext = getExpenses.HasNext,
+            //     HasPrevious = getExpenses.HasPrevious
+            // });
             var shapedData = expensesShapedData as ExpandoObject[] ?? expensesShapedData.ToArray();
-            return await Task.FromResult(Result<IEnumerable<ExpandoObject>, Exception>.SucceedWith(shapedData.Append(obj)));
+            return await Task.FromResult(Result<IEnumerable<ExpandoObject>, Exception>.SucceedWith(shapedData));
         }
     }
 }
